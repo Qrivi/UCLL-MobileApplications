86d714d7e88bc999f6db7216e651f5a76630e2b4
Kristof Dewilde <Krivi@rMBP15.local>
2015-10-21 02:55:41 +0200

    Assignment pretty much completed. The views for portrait and landscape have been shaped and the code is fully working... but there's a weird bug when resetting the stopwatch. Bug will be addressed after a good night of tea and a hot cup of sleep. Also still to do is the OnResume() and OnPause(). But apart from that, today was a good day.

--- Chronometer/Chronometer/MainActivity.cs
+++ Chronometer/Chronometer/MainActivity.cs
@@ -15,17 +15,23 @@ namespace Chronometer
 	{
 		
 		TextView timeView;
-		Button startStopButton;
-		Button resetButton;
+		Button startStopButton,
+			pauseButton,
+			resetButton;
 
 		ISharedPreferences data;
 		ISharedPreferencesEditor editor;
 
 		Handler timeHandler;
-		DateTime startTime;
-		TimeSpan accumulatedTime;
+		DateTime startTime, 
+			stopTime;
+		TimeSpan accumulatedTime,
+			accumulatedStoppedTime;
 
-		bool isRunning = false;
+		int state = 0; 	
+		// 0: stopped
+		// 1: started
+		// 2: paused
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -44,14 +50,18 @@ namespace Chronometer
 			// and attach an event to it
 			timeView = FindViewById<TextView> (Resource.Id.timeView);
 			startStopButton = FindViewById<Button> (Resource.Id.startStopButton);
+			pauseButton = FindViewById<Button> (Resource.Id.pauseButton);
 			resetButton = FindViewById<Button> (Resource.Id.resetButton);
-			
+
 			startStopButton.Click += (sender, e) => StartStopHandler();
+			pauseButton.Click += (sender, e) => PauseHandler();
 			resetButton.Click += (sender, e) => ResetHandler();
 
+			pauseButton.Enabled = false;
+
 			timeHandler = new Handler ();
-			startTime = new DateTime ();
-			accumulatedTime = new TimeSpan ();
+			startTime = stopTime = new DateTime ();
+			accumulatedTime = accumulatedStoppedTime = new TimeSpan ();
 		}
 
 		protected override void OnResume(){
@@ -64,11 +74,11 @@ namespace Chronometer
 		protected override void OnPause(){
 			base.OnPause ();
 
-			isRunning = true;
-			StartStopHandler ();
+			//isRunning = true;
+			//StartStopHandler ();
 
-			editor.PutString ("count", accumulatedTime.ToString() );
-			editor.Apply ();
+			//editor.PutString ("count", accumulatedTime.ToString() );
+			//editor.Apply ();
 		}
 
 		/* protected override void OnSaveInstanceState(Bundle outState) {
@@ -86,35 +96,54 @@ namespace Chronometer
 		private void ResetHandler(){
 			startTime = new DateTime();
 			accumulatedTime = new TimeSpan ();
-			timeView.Text = accumulatedTime.ToString(@"mm\:ss\,fff");
+			timeView.Text = accumulatedTime.ToString(@"mm\:ss\.fff");
+		}
+
+		private void PauseHandler(){
+			state = 2; // now paused
+			pauseButton.Enabled = false;
+			resetButton.Enabled = false;
+			startStopButton.Text = GetString (Resource.String.start);
 		}
 
 		private void StartStopHandler(){
-			Log.Info ("Chronometer", "startStopHandler()");
-
-			if (isRunning) {
-				isRunning = false;
-				resetButton.Enabled = true;
-				startStopButton.Text = GetString (Resource.String.start);
-			} else {
-				isRunning = true;
+			switch (state) {
+			case 0:
+			case 2:
+				state = 1; // now resumed/started
+				pauseButton.Enabled = true;
 				resetButton.Enabled = false;
 				startStopButton.Text = GetString (Resource.String.stop);
 
 				if (startTime == DateTime.MinValue)
 					startTime = DateTime.Now;
+				if (stopTime != DateTime.MinValue) {
+					startTime = startTime.Add( DateTime.Now.Subtract (stopTime) );
+					accumulatedTime = accumulatedTime.Subtract (accumulatedStoppedTime);
+					stopTime = new DateTime();
+				}
+
 				TriggerUpdate ();
+				break;
+			case 1:
+				state = 0; // now stopped
+				pauseButton.Enabled = false;
+				resetButton.Enabled = true;
+				startStopButton.Text = GetString (Resource.String.start);
+
+				stopTime = DateTime.Now;
+				break;
 			}
 		}
 
 		private void TriggerUpdate(){
-			if(isRunning) timeHandler.PostDelayed (Update, 1);
+			timeHandler.PostDelayed (Update, 1);
 		}
 		private void Update(){
 			accumulatedTime = DateTime.Now.Subtract( startTime );
-			timeView.Text = accumulatedTime.ToString(@"mm\:ss\,fff");
+			timeView.Text = accumulatedTime.ToString(@"mm\:ss\.fff");
 
-			TriggerUpdate ();
+			if(state == 1) TriggerUpdate ();
 		}
 	}
 }
