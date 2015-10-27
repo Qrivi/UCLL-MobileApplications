f0ec63ecefd646e477bdaf8f0c048c88869c0099
Kristof Dewilde <Krivi@MBP15.local>
2015-10-27 01:23:48 +0100

    Huge commitgit add --all Dictionary The entire first view is basically done (that's like half the assignment), including some nice personal touches, squashed bugs everywhere and a little yet fabulous icon for all viewports.

--- Dictionary/Dictionary/MainActivity.cs
+++ Dictionary/Dictionary/MainActivity.cs
@@ -8,61 +8,114 @@ using Android.Widget;
 using Android.OS;
 using System.Net;
 using System.Collections.Specialized;
+using System.Linq;
+using System.IO;
 using System.Xml;
+using System.Collections.Generic;
+using Android.Util;
 
 namespace Dictionary
 {
-	[Activity (Label = "Dictionary", MainLauncher = true, Icon = "@drawable/icon")]
+	[Activity (Label = "Dinky Dictionary", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
 
-		EditText searchField;
-		ImageButton searchButton;
-		ListView wordList;
+		EditText SearchField;
+		ImageButton SearchButton;
+		TextView SearchInfo;
+		ListView WordList;
 
-		WebClient client;
-		Uri uri;
-		NameValueCollection parameters;
-		XmlDocument response;
+		WordListAdapter Adapter;
 
-		String[] test = { "yolo", "swag", "wafa", "jan" };
+		WebClient Client;
+		Uri Uri;
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
+			Log.Info ("Dictionary", "Hello World!");
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			searchField = FindViewById<EditText> (Resource.Id.searchField);
-			searchButton = FindViewById<ImageButton> (Resource.Id.searchButton);
-			wordList = FindViewById<ListView> (Resource.Id.wordList);
+			SearchField = FindViewById<EditText> (Resource.Id.SearchField);
+			SearchButton = FindViewById<ImageButton> (Resource.Id.SearchButton);
+			SearchInfo = FindViewById<TextView> (Resource.Id.SearchInfo);
+			WordList = FindViewById<ListView> (Resource.Id.WordList);
 
-			client = new WebClient();
-			uri = new Uri ("http://services.aonaware.com/DictService/DictService.asmx/Define");
-			parameters= new NameValueCollection();
+			SearchButton.Enabled = false;
 
-			parameters.Add ("word", "waffle");
+			SearchField.AfterTextChanged += (sender, e) => SearchHandler();
+			SearchButton.Click += (sender, e) => SearchHandler();
 
-			client.UploadValuesCompleted += populateWordList;
-			client.UploadValuesAsync (uri, parameters);
+			Client = new WebClient();
 
-
-			//ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, Resource.Layout.ListItem ,test);
-			//wordList.Adapter = adapter;
+			Uri = new Uri ("http://services.aonaware.com/DictService/DictService.asmx/Define");
 		}
 
-		private void populateWordList( object sender, UploadValuesCompletedEventArgs e){
-			if (e.Error == null) {
-				 
-				response = new XmlDocument ();
-				response.Load (e.Result);
+		private void PopulateWordList( object sender, UploadValuesCompletedEventArgs e){
+			String SearchQuery = SearchField.Text.Trim ();
+
+			if (SearchQuery != "") {
+				if (e.Error == null) {
+					 
+					List<SearchResult> Results = new List<SearchResult> ();
+					XmlDocument Response = new XmlDocument ();
+
+					Response.Load (new MemoryStream (e.Result));
+				
+					XmlNodeList Definitions = Response.GetElementsByTagName ("Definition");
+
+					SearchInfo.Text = Definitions.Count + " definitions for “" + SearchQuery + "”.";
+					if(Definitions.Count == 1 )
+						SearchInfo.Text = Definitions.Count + " definition for “" + SearchQuery + "”.";
+
+					foreach (XmlNode d in Definitions) {
+						//String dictionary = definition["Dictionary"]["Name"].InnerText;
+						//String worddefinition = definition["WordDefinition"].InnerText;
+						//Console.WriteLine ("Element: " + dictionary + " " + worddefinition);
+						Results.Add (new SearchResult {
+							Id = Results.Count,
+							Dictionary = d ["Dictionary"] ["Name"].InnerText,
+							Definition = d ["WordDefinition"].InnerText
+						});
+					}
+
+					Adapter = new WordListAdapter (this, Results);
+					WordList.Adapter = Adapter;
+
+				} else {
+					SearchInfo.Text = "Looking for definitions...";
+				}
 			} else {
-				Console.WriteLine ("Bush did 9/11");
+				SearchInfo.Text = "";
 			}
 		}
 
+		private void SearchHandler(){
+			Log.Info ("Dictionary", "SearchHandler()");
+
+			SearchButton.Enabled = ( SearchField.Text.Trim() == "" ) ? false : true;
+
+			NameValueCollection Parameters = new NameValueCollection();
+
+			Parameters.Add ( "word", SearchField.Text.Trim() );
+
+			Client.UploadValuesCompleted += PopulateWordList;
+			Client.CancelAsync ();
+			try{
+				Client.UploadValuesAsync (Uri, Parameters);
+			}catch(Exception e){
+				// fuck the police
+				Log.Info ("MainActivity", "SearchHandler() -> EXCEPTION CAUGHT");
+			}
+		}
+	
+	}
+
+	public class SearchResult{
+		public long Id{ get; set;}
+		public String Dictionary{ get; set;}
+		public String Definition{ get; set;}
 	}
 }
-
-
---
+++ Dictionary/Dictionary/WordListAdapter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Android.Widget;
+using Android.App;
+using System.Collections.Generic;
+using Android.Views;
+using Android.Util;
+
+namespace Dictionary
+{
+	public class WordListAdapter : BaseAdapter 
+	{
+		Activity activity;
+		List<SearchResult> results;
+
+		public WordListAdapter( Activity a, List<SearchResult> r )
+		{
+			Log.Info ("WordListAdapter", "Initialized - results to show: " + r.Count);
+			
+			activity = a;
+			results = r;
+		}
+
+		public override int Count {
+			get { return results.Count; }
+		}
+
+		public override Java.Lang.Object GetItem (int position) {
+			// could wrap a Contact in a Java.Lang.Object
+			// to return it here if needed
+			return null;
+		}
+
+		public override long GetItemId (int position) {
+			return results [position].Id;
+		}
+
+		public override View GetView (int position, View convertView, ViewGroup parent)
+		{
+			var view = convertView ?? activity.LayoutInflater.Inflate (
+				Resource.Layout.ListItem, parent, false);
+			var dictionary = view.FindViewById<TextView> (Resource.Id.Dictionary);
+			var definition = view.FindViewById<TextView> (Resource.Id.Definition);
+
+			dictionary.Text = results [position].Dictionary;
+			definition.Text = results [position].Definition;
+
+			return view;
+		}
+	}
+}
+
