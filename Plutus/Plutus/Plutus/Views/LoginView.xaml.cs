12d819b6cc5a457d25e01645afe801c993ffa326
Kristof Dewilde <Krivi@rMBP15.lan>
2015-11-10 18:47:07 +0100

    Added final project

---
+++ Plutus/Plutus/Plutus/Views/LoginView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Plutus
+{
+	public partial class LoginView : ContentPage
+	{
+		public LoginView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
